20f30aa83d8f557b036e5ca61af737f6b939480b
Touqeer Ansari <touqeeransari001@gmail.com>
2024-07-12 22:51:16 +0530

    Practics

--- Pattern question/123.cpp
+++ Pattern question/123.cpp
@@ -1,26 +1,19 @@
-
-// Question
-// 111
-// 222
-// 333
-// 444
-
 #include <stdio.h>
 #include <iostream>
 
 using namespace std;
 int main()
 {
-
     for (int i = 1; i <= 4; i++)
-    {
-        for (int j = 1; j <= 3; j++)
-        {
 
-            cout << i;
+    {
+        for (int j = 4; j > i; j--)
+
+        {
+            cout << 4 - j + 1;
         }
+
         cout << endl;
-        
     }
 
     return 0;
