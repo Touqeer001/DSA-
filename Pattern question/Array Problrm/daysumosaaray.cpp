25e5682c2329fcb88cbf32ac561084fa11186d33
Touqeer Ansari <touqeeransari001@gmail.com>
2024-07-15 14:39:16 +0530

    day 3

---
+++ Pattern question/Array Problrm/daysumosaaray.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a[] = {1, -2, 3, 4, 5};
+    int size = sizeof(a) / sizeof(a[0]);
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum = sum + a[i];
+    }
+    cout << sum;
+
+    return 0;
+}
