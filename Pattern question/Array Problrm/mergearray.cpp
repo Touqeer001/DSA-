f2a75aad451ba1bfaa538f7491c1a1c228987454
Touqeer Ansari <touqeeransari001@gmail.com>
2024-07-17 22:50:53 +0530

    day 9

---
+++ Pattern question/Array Problrm/mergearray.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int arr1[4] = {1, 2, 3, 4};
+    int arr2[4] = {4, 5, 6, 7};
+    int sum = arr1[0] + arr2[1];
+
+    cout << sum;
+
+    return 0;
+}
