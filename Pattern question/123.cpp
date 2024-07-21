7a49411fa227261f65abac023ab807004a022482
Touqeer Ansari <touqeeransari001@gmail.com>
2024-07-21 22:16:54 +0530

    daily practics

--- Pattern question/123.cpp
+++ Pattern question/123.cpp
@@ -4,17 +4,8 @@
 using namespace std;
 int main()
 {
-    for (int i = 1; i <= 4; i++)
-
-    {
-        for (int j = 4; j > i; j--)
-
-        {
-            cout << 4 - j + 1;
-        }
-
-        cout << endl;
-    }
+    int a=10;
+    cout<< a;
 
     return 0;
 }
